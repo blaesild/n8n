4ba27c9aa9ecd7db6eb5cce1e2e22d23d81c215f
blaesild <seb.krogh@gmail.com>
2024-10-12 11:15:30 +0200

    Delete docker/images/n8n-custom/n8n-nodes-elevenlabs-main directory

--- docker/images/n8n-custom/n8n-nodes-elevenlabs-main/nodes/ElevenLabs/methods/debugRequest.ts
+++
@@ -1,9 +0,0 @@
-import { IExecuteSingleFunctions, IHttpRequestOptions } from 'n8n-workflow';
-
-export async function debugRequest(
-	this: IExecuteSingleFunctions,
-	requestOptions: IHttpRequestOptions,
-): Promise<IHttpRequestOptions> {
-	console.log(requestOptions);
-	return requestOptions;
-}
